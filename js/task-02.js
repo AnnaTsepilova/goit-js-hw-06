1648794aec9b79f9d67114766e5b6d2359da21a4
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-14 21:46:57 +0200

    task 1, 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const makeIngredientsItems = ingredients => {
+  
+  return ingredients.map(ingredient => {
+    
+    const liRef = document.createElement('li');
+    liRef.classList.add('item');
+    liRef.textContent = ingredient;
+
+    return liRef;
+  });
+};
+
+const elements = makeIngredientsItems(ingredients);
+
+ingredientsListRef.append(...elements); 
