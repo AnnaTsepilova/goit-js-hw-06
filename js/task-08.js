fbb55ff523c726257ea24e75589bc5ddd5e2b173
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-16 18:22:14 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email,
+        password,
+    }
+
+    console.log(formData);
+
+    if (email === "" || password === "") {
+        alert("Будь ласка, заповніть всі поля!");
+    }
+
+    event.currentTarget.reset();
+}
 
