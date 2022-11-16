3587bed0f485cf40d517e745e07f09f899cbd5ec
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-16 18:47:38 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const hexColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  hexColor.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
